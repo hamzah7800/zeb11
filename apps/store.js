f71847e2b76d5a0a4f936a4a70be33208f0452c6
hamzah7800 <hzeb7800pass@gmail.com>
2025-05-20 19:28:28 +0100

    Update store.js

--- apps/store.js
+++ apps/store.js
@@ -1,1 +1,8 @@
-// Zeb Store stub
+<!DOCTYPE html>
+<html>
+<head><title>Zeb Store</title></head>
+<body style="font-family:sans-serif; background:#fff;">
+  <h1>Zeb Store</h1>
+  <p>Welcome to the Zeb11 App Store (coming soon).</p>
+</body>
+</html>
